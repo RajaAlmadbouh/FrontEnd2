ba1ef5c14b1e6e174a2fb9caf1f53f55ec53f1b9
RajaAlmadbouh <146444351+RajaAlmadbouh@users.noreply.github.com>
2023-10-25 11:53:08 +0300

    Js 1

---
+++ js/skills.js
@@ -0,0 +1,23 @@
+//alert("raja");
+
+var name = "Raja";
+let age = 24 ; 
+const isMarried = false;
+const salary = 1000.5;
+const skills = ["html","css","js"];
+var parson = {
+    name: "Raja",
+    age: 25,
+    isMarried:false,
+    salary:1000.5,
+    skills:["html","css","js"]
+
+};
+
+console.log(typeof(name),"name : ",name);
+console.log(typeof(age),"age : ",age);
+console.log(typeof(isMarried),"isMarried : ",isMarried);
+console.log(typeof(salary),"salary : ",salary);
+console.log(typeof(skills),"skills : ",skills);
+console.log(typeof(parson),"parson : ",parson);
+
