8b3f35cb2d314c3caab19d7efff2514660455766
RajaAlmadbouh <146444351+RajaAlmadbouh@users.noreply.github.com>
2023-10-28 11:51:10 +0300

    JS Change Color

--- js/skills.js
+++ js/skills.js
@@ -21,3 +21,25 @@ console.log(typeof(salary),"salary : ",salary);
 console.log(typeof(skills),"skills : ",skills);
 console.log(typeof(parson),"parson : ",parson);
 
+function calculate (num1,num2){
+    let sum=num1+num2;
+    console.log(sum);
+    return sum;
+}
+
+console.log(calculate(20,23));
+calculate(30,20);
+//console.log(sum);
+
+var body=document.getElementsByTagName('body')
+//console.log(body[0].style.backgroundColor);
+
+function changeBackground(){
+    if(body[0].style.backgroundColor=='red'){
+        body[0].style.backgroundColor='pink';
+    }
+    else{
+        body[0].style.backgroundColor='red';
+    }
+}
+
