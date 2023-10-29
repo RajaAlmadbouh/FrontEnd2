3abd54734fcf2190369081b50b9685be7c6d83c6
RajaAlmadbouh <146444351+RajaAlmadbouh@users.noreply.github.com>
2023-10-29 22:35:41 +0300

    The wepsite color changes from light to dark

--- js/skills.js
+++ js/skills.js
@@ -31,15 +31,48 @@ console.log(calculate(20,23));
 calculate(30,20);
 //console.log(sum);
 
-var body=document.getElementsByTagName('body')
+var body=document.getElementsByTagName('body');
+var heder= document.getElementById("headerDiv");
+var aLink1= document.getElementById("headerLinks");
+var footer= document.getElementById("footerDiv");
+//var h11 = heder.getElementsByTagName("h1");
+//var aLink2= aLink1.getElementsByTagName("a");
+//console.log(h11[0].style.color);
+//console.log(aLink1.getElementsByTagName("a")[0].style.color);
 //console.log(body[0].style.backgroundColor);
 
 function changeBackground(){
-    if(body[0].style.backgroundColor=='red'){
-        body[0].style.backgroundColor='pink';
+    //Light style
+    if(body[0].style.backgroundColor=='black'){
+        //channge heder
+        body[0].style.backgroundColor='white';
+        heder.style.backgroundColor='#1a1a1a';
+        heder.getElementsByTagName("h1")[0].style.color="#fff";
+        for(let i=0 ; i<aLink1.getElementsByTagName("a").length ; i++){
+            aLink1.getElementsByTagName("a")[i].style.color = "#fff";
+            aLink1.getElementsByTagName("a")[i].style.backgroundColor = "gray";
+            aLink1.getElementsByTagName("a")[i].style.border = "1px solid #fff";
+        }
+        //change footer
+        footer.style.backgroundColor = "#1a1a1a";
+        footer.getElementsByTagName("p")[0].style.color="white";
+
     }
+    //Dark style
     else{
-        body[0].style.backgroundColor='red';
+        //change heder
+        body[0].style.backgroundColor='black';
+        heder.style.backgroundColor='#414048';
+        heder.getElementsByTagName("h1")[0].style.color="#D9D8E6";
+        for(let i=0 ; i<aLink1.getElementsByTagName("a").length ; i++){
+            aLink1.getElementsByTagName("a")[i].style.color = "black"
+            aLink1.getElementsByTagName("a")[i].style.backgroundColor = "#C0BFC4";
+            aLink1.getElementsByTagName("a")[i].style.border = "1px solid black";
+        }
+        //change footer
+        footer.style.backgroundColor = "#414048";
+        footer.getElementsByTagName("p")[0].style.color="#CFCED9";
+
     }
 }
 
