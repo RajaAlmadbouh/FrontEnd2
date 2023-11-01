6ffcbd756a11ec1459b2a83e590d67571a9e88db
RajaAlmadbouh <146444351+RajaAlmadbouh@users.noreply.github.com>
2023-11-01 11:50:02 +0300

    added event lesener

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+var contactForm=document.getElementById("contactForm");
+var nameForm = document.getElementById("name");
+var emailForm = document.getElementById("email");
+var subjectForm = document.getElementById("subject");
+
+contactForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    console.log(e);
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Your name'+ nameForm.value + 'and your email ' + emailForm.value + 'and your subject is '+ subjectForm.value,
+        showConfirmButton: false,
+        timer: 7000
+      });
+    }
+
+);
