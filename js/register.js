a0c04c8cff970829f7a05532572db6e590789596
RajaAlmadbouh <146444351+RajaAlmadbouh@users.noreply.github.com>
2023-11-10 13:22:18 +0300

    .

--- js/register.js
+++ js/register.js
@@ -1,13 +1,11 @@
 var formRegister=document.getElementById("formRegister");
-var formName=document.getElementById("form3Example1c").hide;
+var formName=document.getElementById("form3Example1c");
 var formEmail=document.getElementById("form3Example3c");
 var formPass=document.getElementById("form3Example4c");
 var formRepPass=document.getElementById("form3Example4cd");
  
-({
+formName.style.display = "none";
 
-
-})
 formRegister.addEventListener('submit',function(e){
     e.preventDefault();
     if(formPass.value != formRepPass.value){
