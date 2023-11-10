3524669f3c76e2a3fba5ed7e2325ce8b3fd007bf
RajaAlmadbouh <146444351+RajaAlmadbouh@users.noreply.github.com>
2023-11-10 15:55:37 +0300

    .

--- js/register.js
+++ js/register.js
@@ -3,8 +3,9 @@ var formName=document.getElementById("form3Example1c");
 var formEmail=document.getElementById("form3Example3c");
 var formPass=document.getElementById("form3Example4c");
 var formRepPass=document.getElementById("form3Example4cd");
+var usersData = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')) : [] ;
  
-formName.style.display = "none";
+
 
 formRegister.addEventListener('submit',function(e){
     e.preventDefault();
@@ -20,17 +21,55 @@ formRegister.addEventListener('submit',function(e){
 
     }//end if
     else{
-        localStorage.setItem('name',formName.value);
-        localStorage.setItem('email',formEmail.value);
-        localStorage.setItem('password',formPass.value);
+        //localStorage.setItem('name',formName.value);
+        //localStorage.setItem('email',formEmail.value);
+        //localStorage.setItem('password',formPass.value);
 
-        Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: 'Your account is created . ',
-            showConfirmButton: false,
-            timer: 7000
-          });
+        let user = {
+            name : formName.value,
+            email : formEmail.value,
+            password : formPass.value
+        };
+
+        for(let i=0 ; usersData.length ; i++){
+            if(usersData[i]['email'] == formEmail.value){
+
+                Swal.fire({
+                    position: 'center',
+                    icon: 'error',
+                    title: 'your Email is exiest',
+                    showConfirmButton: false,
+                    timer: 7000
+                  
+                  });//end Swet Alert
+                  return ; 
+            }
+            else if (usersData[i]['name'] == formName.value){
+                Swal.fire({
+                    position: 'center',
+                    icon: 'error',
+                    title: 'your Name is exiest',
+                    showConfirmButton: false,
+                    timer: 7000
+                  
+                  });//end Swet Alert
+                  return ; 
+            }
+            else{
+                usersData.push(user);
+                localStorage.setItem("users",JSON.stringify(usersData));
+
+                Swal.fire({
+                 position: 'center',
+                 icon: 'success',
+                 title: 'Your account is created . ',
+                 showConfirmButton: false,
+                 timer: 7000
+              });
+            }//end else
+        }//end for
+
+        
     }
 });
 
